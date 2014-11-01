f6c3cdcdfc2b351d1841b0a05345996d144c9836
Tony <tony@aplawrence.com>
2014-11-01 09:36:47 -0400

    first commit

---
+++ Taskit/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Taskit
+//
+//  Created by Tony on 10/31/14.
+//  Copyright (c) 2014 A.P. Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
